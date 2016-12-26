85ac3380239f617b0054a34ac25b5004e35b6032
avridev <surfbear7d@gmail.com>
2016-12-26 06:03:30 +0000

    connect mongoDB

--- app.js
+++ app.js
@@ -4,17 +4,25 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require("mongoose");
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
+// connect db
+var connect = mongoose.connect('mongodb://localhost/nest');
+var db = connect.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+    console.log("connect successfully")
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
